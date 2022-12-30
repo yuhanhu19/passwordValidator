4dc2bdd3642a36970c4aa6f077c7c6b276cfb669
Yuhan Hu <yuhan.hu@cat.com>
2022-12-30 16:26:15 +0800

    [Yuhan] test: add test ShouldReturnErrorWhenPasswordContainsFewerThan2Numbers

--- passwordValidator/passwordValidator/Tests.cs
+++ passwordValidator/passwordValidator/Tests.cs
@@ -14,5 +14,15 @@ namespace passwordValidator
             Assert.Equal("Password must be at least 8 characters", validationResult.Message);
 
         }
+        
+        [Fact]
+        public void ShouldReturnErrorWhenPasswordContainsFewerThan2Numbers()
+        {
+            var passwordValidator = new PasswordValidator();
+            var validationResult = passwordValidator.Validate("abcdefgh1");
+            Assert.False(validationResult.Passed);
+            Assert.Equal("The password must contain at least 2 numbers", validationResult.Message);
+
+        }
     }
 }
