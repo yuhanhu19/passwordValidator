2b3ee36fa64a26777c00242f5ac808fa7f105f7b
Yuhan Hu <yuhan.hu@cat.com>
2022-12-30 16:20:58 +0800

    [Yuhan] feat: implement validation for password length

--- passwordValidator/passwordValidator/PasswordValidator.cs
+++ passwordValidator/passwordValidator/PasswordValidator.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Xml.Schema;
+
 namespace passwordValidator
 {
     public class PasswordValidator
     {
-        
+        public ValidationResult Validate(string password)
+        {
+            if (password.Length < 8)
+            {
+                return new ValidationResult(false, "Password must be at least 8 characters");
+            }
+
+            return new ValidationResult(true, "none");
+        }
     }
 }
