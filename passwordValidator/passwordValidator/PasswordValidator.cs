2c37b4fc2028e5fd284961075eb5b21f6990f56d
Yuhan Hu <yuhan.hu@cat.com>
2022-12-30 16:52:59 +0800

    [Yuhan] feat: implement validation for number count in password

--- passwordValidator/passwordValidator/PasswordValidator.cs
+++ passwordValidator/passwordValidator/PasswordValidator.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Schema;
 
 namespace passwordValidator
@@ -12,7 +15,23 @@ namespace passwordValidator
                 return new ValidationResult(false, "Password must be at least 8 characters");
             }
 
+            if (CountNumbersInPassword(password) < 2)
+            {
+                return new ValidationResult(false, "The password must contain at least 2 numbers");
+
+            }
+
             return new ValidationResult(true, "none");
+        }
+
+        private int CountNumbersInPassword(string password)
+        {
+            return password.Where(IsNumber).ToList().Count;
+        }
+
+        private bool IsNumber(char c)
+        {
+            return int.TryParse(c.ToString(), out _);
         }
     }
 }
